d7bc7fb031a3a30f44bf9b4589318e94462fdfc1
jack-parsons <jack.parsons.uk@icloud.com>
2021-02-15 22:27:29 +0000

    Dodgy knn

--- src/models/knn.py
+++ src/models/knn.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 from src.data import TestDataset, TrainDataset
 from src import ModelABC
 
@@ -8,17 +10,48 @@ class KNNModel(ModelABC):
         self.users = {}
 
     def _add_user_rating(self, user_id, product, rating):
+        if rating == "rating":
+            return
         if user_id not in self.users:
-            self.users[user_id] = {product: rating}
+            self.users[user_id] = {product: float(rating[0])}
         else:
-            self.users[user_id][product] = rating
+            self.users[user_id][product] = float(rating[0])
+
+    @staticmethod
+    def cosine_similarity(a, b):
+        s = a_sum = b_sum = 0
+        for item in {**a, **b}.keys():
+            s += a.get(item, 0) * b.get(item, 0)
+            a_sum += a.get(item, 0)
+            b_sum += b.get(item, 0)
+        return s/(a_sum*b_sum)
 
     def train(self, dataset: TrainDataset):
         # Add all users to user dict
-        self.users = {}
-        for (i, features), rating in zip(dataset.X.iterrows(), dataset.Y.iterrows()):
-            self._add_user_rating(features["user id"], features["item id"], rating)
-        print(self.users)
+        X = dataset.X.values
+        Y = dataset.Y.values
+
+        # Get all user ratings
+        for (user_id, item_id, timestamp), rating in zip(X, Y):
+            self._add_user_rating(user_id, item_id, rating)
+        # users := UserID + ProductID -> rating
+
+        # for item in
+
+        # Calculate similarity Item A, and all other Items
+        # Weighted mean of ratings (e.g. 3.5/5) for those items, based on similarity
+        #
+        # for item in items: SUM (SUM(for user in users: similarity(user, this_user)*Y[Item][user].rating) * similarity(item, this_item))
 
     def predict(self, dataset: TestDataset):
-        pass
+        results = []
+        for (user_a, item_a, timestamp) in dataset.dataset.values:
+            unweighted = 0
+            total_weights = 0
+            for user_b in self.users.keys():
+                if item_a in self.users[user_b]:
+                    sim = self.cosine_similarity(self.users[user_a], self.users[user_b])
+                    unweighted += sim * self.users[user_b][item_a]
+                    total_weights += sim
+            results.append(unweighted/total_weights)
+        return np.array(results)
