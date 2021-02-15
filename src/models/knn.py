f28f51510870c4d95e54394ae26e80da31ddd2a4
jack-parsons <jack.parsons.uk@icloud.com>
2021-02-15 18:18:03 +0000

    KNN users (not working)

--- src/models/knn.py
+++ src/models/knn.py
@@ -3,8 +3,22 @@ from src import ModelABC
 
 
 class KNNModel(ModelABC):
+
+    def __init__(self):
+        self.users = {}
+
+    def _add_user_rating(self, user_id, product, rating):
+        if user_id not in self.users:
+            self.users[user_id] = {product: rating}
+        else:
+            self.users[user_id][product] = rating
+
     def train(self, dataset: TrainDataset):
-        pass
+        # Add all users to user dict
+        self.users = {}
+        for (i, features), rating in zip(dataset.X.iterrows(), dataset.Y.iterrows()):
+            self._add_user_rating(features["user id"], features["item id"], rating)
+        print(self.users)
 
     def predict(self, dataset: TestDataset):
         pass
