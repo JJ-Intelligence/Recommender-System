8ffe1185615a6026aed7ee3e00470a5c75899803
jack-parsons <jack.parsons.uk@icloud.com>
2021-02-19 15:41:33 +0000

    Changing approach

--- src/models/matrix_fact.py
+++ src/models/matrix_fact.py
@@ -1,17 +1,9 @@
-from abc import ABC, abstractmethod
-
 import numpy as np
-from numpy import linalg as LA
 
-from src import TrainDataset
-
-
-class CustomMatrix(ABC):
-    def __init__(self, shape):
-        self.shape = shape
+from src import TrainDataset, ModelABC, TestDataset
 
 
-class SparseMatrix(CustomMatrix):
+class DictMatrix:
 
     def __init__(self, dataset: TrainDataset):
         """
@@ -19,95 +11,35 @@ class SparseMatrix(CustomMatrix):
 
         user id + item id -> rating
         """
-        self.users = {}
-        self.max_item = 0
-        self.max_user = 0
+        self.users_ratings = {}
+        self.user_map = {}
+        self.item_map = {}
 
         for i, (user_id, item_id, timestamp, rating) in dataset.dataset.iterrows():
-            self.max_item = max(self.max_item, item_id)
-            self.max_user = max(self.max_user, user_id)
-            if user_id not in self.users:
-                self.users[user_id] = {item_id: rating}
-            else:
-                self.users[user_id][item_id] = rating
 
-        super().__init__((self.get_width(), self.get_height()))
+            # Adding ratings for each (user, item) pair - if the user doesn't exist, it will add it.
+            self.users_ratings.setdefault(user_id, {})[item_id] = rating
 
-    def get_height(self):
-        """The height matters rather than the number of users, since we're not mapping users to indices"""
-        return self.max_user
+            self.user_map.setdefault(user_id, len(self.user_map))
+            self.item_map.setdefault(item_id, len(self.item_map))
 
-    def get_width(self):
-        """The width matters rather than the number of items, since we're not mapping items to indices"""
-        return self.max_item
+    def num_users(self):
+        return len(self.user_map)
 
-    def get(self, user_id, item_id):
-        if user_id in self.users:
-            return self.users.get(user_id).get(item_id, 0)
-        return 0
-
-    def get_item_vec(self, user_id):
-        vec = np.zeros(self.max_item)
-
-        if user_id in self.users:  # if the user doesn't exist, return row of 0s
-            user_items = self.users.get(user_id)
-            for item_id in user_items:
-                vec[item_id] = user_items[item_id]
-
-        return vec
+    def num_items(self):
+        return len(self.item_map)
 
 
-class LazyMatrix(CustomMatrix):
-    def __init__(self, generator, shape):
-        super().__init__(shape)
-        self.generator = generator
+class MatrixFactoriser(ModelABC):
+    def __init__(self, k: float, pq_init: int):
+        self.k = k
+        self.pq_init = pq_init
 
-    def __call__(self, *args, **kwargs):
-        return np.asarray(list(self.generator))
+    def train(self, dataset: TrainDataset):
+        R = DictMatrix(dataset)
 
-    def __iter__(self):
-        return self.generator.__iter__()
+        P = np.full((R.num_users(), self.k), self.pq_init)
+        Q = np.full((self.k, R.num_items()), self.pq_init)
 
-    def __next__(self):
-        return self.generator.__next__()
-
-    @staticmethod
-    def lazy_dot(P: np.ndarray, Q: np.ndarray):
-        """Lazy eval of P.Q"""
-        matrix = LazyMatrix(
-            (np.asarray([np.dot(r, Q[:, i]) for i in range(Q.shape[-1])]) for r in P),
-            (P.shape[0], Q.shape[-1])
-        )
-        assert matrix.shape == (P.shape[0], Q.shape[-1])
-        return matrix
-
-    @staticmethod
-    def lazy_sub(R: SparseMatrix, PQ):
-        """Lazy eval of R - PQ"""
-        assert R.shape == PQ.shape
-        matrix = LazyMatrix((np.subtract(R.get_item_vec(i + 1) - row) for i, row in enumerate(PQ)), PQ.shape)
-        assert matrix.shape == PQ.shape
-        return matrix
-
-    @staticmethod
-    def lazy_pow(P, power: int):
-        """ Raises each value in matrix P to the given power"""
-        return LazyMatrix((row ** power for row in P.generator), P.shape)
-
-    @staticmethod
-    def lazy_mean(P):
-        return LazyMatrix((np.mean(row) for row in P), (P.shape[0],))
-
-
-class MatrixFactoriser:
-    def __init__(self, R: SparseMatrix):
-        """
-        P.Q ~= R
-        """
-
-    @staticmethod
-    def loss_mse(R: SparseMatrix, P: np.array, Q: np.array, l: int):
-        """Returns the MSE loss for matrix factorization, where R ~= P.Q"""
-        return \
-            np.mean(LazyMatrix.lazy_pow(LazyMatrix.lazy_sub(R, LazyMatrix.lazy_dot(P, Q)), 2), axis=0) \
-            + l * (LA.norm(P) ** 2 + LA.norm(Q) ** 2)
+    def predict(self, dataset: TestDataset) -> np.ndarray:
+        pass
--- tests/test_matrix_fact.py
+++ tests/test_matrix_fact.py
@@ -1,52 +1,27 @@
 import numpy as np
+import pandas as pd
+from pytest import fixture
 
-from models.matrix_fact import LazyMatrix, SparseMatrix
-
-
-def test_lazy_dot_same_size():
-    A = np.array([[1, 2],
-                  [-2, 3]])
-    B = np.array([[3, 4],
-                  [0, 1]])
-    assert np.array_equal(A.dot(B), LazyMatrix.lazy_dot(A, B)())
+from models.matrix_fact import DictMatrix
+from src import TrainDataset
 
 
-def test_lazy_dot_different_sizes():
-    A = np.array([[1, 2],
-                  [-2, 3],
-                  [9, -1]])
-    B = np.array([[3, 4.3, 7],
-                  [0, 1, 1.2]])
-    assert np.array_equal(A.dot(B), LazyMatrix.lazy_dot(A, B)())
+@fixture
+def dataset():
+    dataset = pd.DataFrame({
+        "user id": [5, 10, 20, 24, 10, 5],
+        "item id": [1, 3, 1, 2, 5, 9],
+        "timestamp": [1, 2, 3, 4, 5, 6],
+    })
+    ratings = pd.Series([1, 4, 0.5, 4.5, 3, 2.5])
+    return TrainDataset(dataset, ratings)
 
 
-def test_lazy_dot_large():
-    A = np.ones(100).reshape(-1, 1)
-    B = np.ones(100).reshape(-1, 1).T
-    assert np.array_equal(A.dot(B), LazyMatrix.lazy_dot(A, B)())
+@fixture
+def dict_matrix(dataset):
+    return DictMatrix(dataset)
 
 
-def test_lazy_dot_larger():
-    A = np.ones(1000000).reshape(-1, 1)
-    B = np.ones(1000000).reshape(-1, 1).T
-    C = LazyMatrix.lazy_dot(A, B)
-    assert next(C)[3] == 1
-    assert next(C)[4] == 1
-
-
-def test_lazy_dot_even_larger():
-    k = 200
-    A = np.ones(600000).reshape(-1, k)
-    B = np.ones(90000).reshape(-1, k).T
-    C = LazyMatrix.lazy_dot(A, B)
-    assert next(C)[3] == k
-    assert next(C)[4] == k
-
-
-def test_lazy_sub():
-    A = SparseMatrix()
-    B = LazyMatrix((np.array([[3, 4], [0, 1]])), (2, 2))
-    assert np.array_equal(np.subtract(A, B), LazyMatrix.lazy_sub(A, B)())
-
-def test_matrix_mapper():
-    pass
+def test_dict_matrix_size(dict_matrix):
+    assert dict_matrix.num_items() == 5
+    assert dict_matrix.num_users() == 4
