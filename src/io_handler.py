581f7c5da0aa7a7b0c82a30f90141040d99a83d9
jack-parsons <jack.parsons.uk@icloud.com>
2021-02-20 14:47:50 +0000

    Improving running setup

--- src/io_handler.py
+++ src/io_handler.py
@@ -1,4 +1,6 @@
+import os
 from typing import List, Tuple
+from pathlib import Path
 import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split
@@ -6,7 +8,10 @@ from sklearn.model_selection import train_test_split
 from src.data import TrainDataset, TestDataset, EvaluationDataset
 
 
-def read_train_csv(filename: str, test_size=0.2) -> (TrainDataset, TrainDataset):
+DEFAULT_RESULTS_FOLDER = "../results/"
+
+
+def read_train_csv(filename: str, test_size=0.1, eval_size=0.1) -> (TrainDataset, EvaluationDataset, EvaluationDataset):
     dataset = _read_csv_to_dataframe(
         filename,
         [("user id", np.int32), ("item id", np.int32), ("user rating", np.float32), ("timestamp", np.float32)]
@@ -15,10 +20,19 @@ def read_train_csv(filename: str, test_size=0.2) -> (TrainDataset, TrainDataset)
     x_train, x_evaluation, y_train, y_evaluation = train_test_split(
         dataset[["user id", "item id", "timestamp"]],
         dataset[["user rating"]],
-        test_size=test_size,
+        test_size=eval_size,
         shuffle=True)
 
-    return TrainDataset(x_train, y_train), EvaluationDataset(x_evaluation, y_evaluation)
+    x_train, x_test, y_train, y_test = train_test_split(
+        x_train,
+        y_train,
+        test_size=test_size/(1-eval_size),  # account for smaller train set
+        shuffle=False)
+
+    return \
+        TrainDataset(x_train, y_train), \
+        EvaluationDataset(x_evaluation, y_evaluation), \
+        EvaluationDataset(x_test, y_test)
 
 
 def read_test_csv(filename: str) -> TestDataset:
