dfd5dfb63eecfaebeb13677c25c615bb5c6aec4c
jack-parsons <jack.parsons.uk@icloud.com>
2021-02-20 22:21:03 +0000

    Adding run.sh

---
+++ run.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+#PBS -l walltime=00:05:00
+
+module load singularity/3.2.0
+export PYTHONPATH="${PYTHONPATH}:/lyceum/jp6g18/Recommender-System/src"
+singularity exec image.sif python src/main --trainfile datasets/comp3208-train-small.csv --testfile datasets/comp3208-test-small.csv --outputfile predictions.csv
