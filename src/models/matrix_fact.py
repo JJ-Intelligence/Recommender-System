f0f63affe7ab0a4755dee56af74335545bd27b58
Josh Hook <joshhook@live.co.uk>
2021-03-19 18:23:27 +0000

    Replacing numpy with tensorflow

--- src/models/matrix_fact.py
+++ src/models/matrix_fact.py
@@ -1,7 +1,11 @@
+from typing import Tuple, List, Union
+
+import tensorflow as tf
 import numpy as np
 from numba import njit, jit
 
 from data import TrainDataset, EvaluationDataset, TestDataset
+from evaluation import Evaluation
 from models.model_base import ModelBase
 from progress_bars import EpochBar
 
@@ -18,35 +22,45 @@ class DictMatrix:
         else:
             self.user_map, self.item_map = maps
 
-        self.users_ratings = np.asarray([
-            [self.user_map[np.int32(row[0])], self.item_map[np.int32(row[1])], row[2]]
-            for row in dataset.dataset.to_numpy()[:, [0, 1, 3]]
-        ])
+        user_indices = []
+        item_indices = []
+        ratings = []
+        for row in dataset.dataset.to_numpy()[:, [0, 1, 3]]:
+            user_indices.append(self.user_map[tf.int32(row[0])])
+            item_indices.append(self.item_map[tf.int32(row[1])])
+            ratings.append(self.user_map[tf.float32(row[2])])
 
-    def num_users(self):
+        self.user_indices = tf.convert_to_tensor(user_indices)
+        self.item_indices = tf.convert_to_tensor(item_indices)
+        self.ratings = tf.convert_to_tensor(ratings)
+
+    def num_users(self) -> int:
         return len(self.user_map)
 
-    def num_items(self):
+    def num_items(self) -> int:
         return len(self.item_map)
 
-    def get_user_item_maps(self):
+    def get_user_item_maps(self) -> Tuple[dict, dict]:
         return self.user_map, self.item_map
 
+    def get_ratings_tensors(self) -> Tuple[tf.Tensor, tf.Tensor, tf.Tensor]:
+        return self.user_indices, self.item_indices, self.ratings
+
     @staticmethod
-    def series_to_index_map(series):
+    def series_to_index_map(series) -> dict:
         return {val: index for index, val in enumerate(series.unique())}
 
 
-def do_train_step(user_indices: np.ndarray,
-                  item_indices: np.ndarray,
-                  ratings: np.ndarray,
-                  user_indices_inv_counts: np.ndarray,
-                  item_indices_inv_counts: np.ndarray,
-                  mu: np.float32,
-                  bu: np.ndarray,
-                  bi: np.ndarray,
-                  H: np.ndarray,
-                  W: np.ndarray,
+def do_train_step(user_indices: tf.Tensor,
+                  item_indices: tf.Tensor,
+                  ratings: tf.Tensor,
+                  user_indices_inv_counts: tf.Tensor,
+                  item_indices_inv_counts: tf.Tensor,
+                  mu: tf.float32,
+                  bu: tf.Tensor,
+                  bi: tf.Tensor,
+                  H: tf.Tensor,
+                  W: tf.Tensor,
                   batch_size: int,
                   lr: float,
                   reg_bu: float,
@@ -55,65 +69,69 @@ def do_train_step(user_indices: np.ndarray,
                   reg_W: float):
     """ Perform a single training step (1 epoch) """
     for i in range(0, len(ratings), batch_size):
-        batch_slice = np.s_[i:i + batch_size]
+        user_indices_batch = user_indices[i:i + batch_size]
+        item_indices_batch = item_indices[i:i + batch_size]
+
         dmse_dbu, dmse_dbi, dmse_dh, dmse_dw = _train_batch(
-            user_indices[batch_slice],
-            item_indices[batch_slice],
-            ratings[batch_slice],
-            user_indices_inv_counts[batch_slice],
-            item_indices_inv_counts[batch_slice],
+            user_indices_batch,
+            item_indices_batch,
+            ratings[i:i + batch_size],
+            user_indices_inv_counts[i:i + batch_size],
+            item_indices_inv_counts[i:i + batch_size],
             mu, bu, bi, H, W, lr, reg_bu, reg_bi, reg_H, reg_W)
 
         # Update weights, using loss gradient changes
+        bu.scatter_nd_add(tf.reshape(user_indices_batch, (-1, 1)), dmse_dbu)
+        bi.scatter_nd_add(tf.reshape(item_indices_batch, (-1, 1)), dmse_dbi)
+        H.scatter_nd_add(tf.reshape(user_indices_batch, (-1, 1)), dmse_dh)
+        W.scatter_nd_add(tf.reshape(item_indices_batch, (-1, 1)), dmse_dw)
+
+
         np.add.at(bu, user_indices[i:i + batch_size], dmse_dbu)
         np.add.at(bi, item_indices[i:i + batch_size], dmse_dbi)
         np.add.at(H, np.s_[user_indices[i:i + batch_size], :], dmse_dh)
         np.add.at(W, np.s_[:, item_indices[i:i + batch_size]], dmse_dw)
 
 
-def indices_to_inv_counts(indices):
-    # Map indices to an array of 1/<index_count>
-    _, inv, counts = np.unique(indices, return_inverse=True, return_counts=True)
-    return np.asarray([1/counts[i] for i in inv])
-
-
-@njit(parallel=True)
-def _train_batch(user_indices: np.ndarray,
-                 item_indices: np.ndarray,
-                 ratings: np.ndarray,
-                 user_inv_counts: np.ndarray,
-                 item_inv_counts: np.ndarray,
-                 mu: np.float32,
-                 bu: np.ndarray,
-                 bi: np.ndarray,
-                 H: np.ndarray,
-                 W: np.ndarray,
+def _train_batch(user_indices: tf.Tensor,
+                 item_indices: tf.Tensor,
+                 ratings: tf.Tensor,
+                 user_inv_counts: tf.Tensor,
+                 item_inv_counts: tf.Tensor,
+                 mu: tf.float32,
+                 bu: tf.Tensor,
+                 bi: tf.Tensor,
+                 H: tf.Tensor,
+                 W: tf.Tensor,
                  lr: float,
                  reg_bu: float,
                  reg_bi: float,
                  reg_H: float,
-                 reg_W: float):
+                 reg_W: float) -> Tuple[tf.Tensor, tf.Tensor, tf.Tensor, tf.Tensor]:
     predictions = _predict_ratings(mu, bu, bi, H, W, user_indices, item_indices)
     residuals = ratings - predictions
 
     # Gradient changes
-    dmse_dbu = lr * ((residuals * user_inv_counts) - (reg_bu * bu[user_indices]))
-    dmse_dbi = lr * ((residuals * item_inv_counts) - (reg_bi * bi[item_indices]))
-    dmse_dh = lr * ((user_inv_counts * residuals * W[:, item_indices]).T - (reg_H * H[user_indices, :]))
-    dmse_dw = lr * ((item_inv_counts * residuals * H[user_indices, :].T) - (reg_W * W[:, item_indices]))
+    dmse_dbu = lr * ((residuals * user_inv_counts) - (reg_bu * tf.gather(bu, user_indices)))
+    dmse_dbi = lr * ((residuals * item_inv_counts) - (reg_bi * tf.gather(bi, item_indices)))
+
+    dmse_dh = lr * (tf.transpose(user_inv_counts * residuals * tf.gather(W, item_indices, axis=0)) -
+                    (reg_H * tf.gather(H, user_indices, axis=1)))
+    dmse_dw = lr * ((item_inv_counts * residuals * tf.transpose(tf.gather(H, user_indices, axis=1))) -
+                    (reg_W * tf.gather(W, item_indices, axis=0)))
     return dmse_dbu, dmse_dbi, dmse_dh, dmse_dw
 
 
-@njit(parallel=True)
-def _predict_ratings(mu: np.float32,
-                     bu: np.ndarray,
-                     bi: np.ndarray,
-                     H: np.ndarray,
-                     W: np.ndarray,
-                     user_indices: np.ndarray,
-                     item_indices: np.ndarray):
-    # Perform a point-wise dot product
-    return mu + bu[user_indices] + bi[item_indices] + np.sum(H[user_indices, :] * W[:, item_indices].T, axis=1)
+def _predict_ratings(mu: tf.float32,
+                     bu: tf.Tensor,
+                     bi: tf.Tensor,
+                     H: tf.Tensor,
+                     W: tf.Tensor,
+                     user_indices: tf.Tensor,
+                     item_indices: tf.Tensor) -> tf.Tensor:
+    # Perform a point-wise matrix dot product
+    return mu + tf.gather(bu, user_indices) + tf.gather(bi, item_indices) + \
+           tf.reduce_sum(tf.gather(H, user_indices, axis=0) * tf.transpose(tf.gather(W, item_indices, axis=1)), axis=1)
 
 
 class MatrixFactoriser(ModelBase):
@@ -139,19 +157,17 @@ class MatrixFactoriser(ModelBase):
         # Latent feature matrices
         norm_mean = 0
         norm_stddev = self.hw_init_stddev
-        self.H = np.random.normal(norm_mean, norm_stddev, (self.R.num_users(), self.k)).astype(np.float32)
-        self.W = np.random.normal(norm_mean, norm_stddev, (self.k, self.R.num_items())).astype(np.float32)
+        self.H = tf.random.normal((self.R.num_users(), self.k), norm_mean, norm_stddev, dtype=tf.dtypes.float32)
+        self.W = tf.random.normal((self.k, self.R.num_items()), norm_mean, norm_stddev, dtype=tf.dtypes.float32)
 
         # Biases (global mean, user bias, item bias)
-        self.mu = np.mean(self.R.users_ratings[:, -1], dtype=np.float32)
-        self.bu = np.zeros(self.R.num_users(), dtype=np.float32)
-        self.bi = np.zeros(self.R.num_items(), dtype=np.float32)
+        self.mu = tf.reduce_mean(self.R.get_ratings_tensors()[-1], dtype=tf.dtypes.float32)
+        self.bu = tf.zeros(self.R.num_users() + 1, dtype=tf.dtypes.float32)  # Extra 0 row for unknown items/users
+        self.bi = tf.zeros(self.R.num_items() + 1, dtype=tf.dtypes.float32)
 
         # Add 0 rows for unknown items or users
-        self.H = np.append(self.H, np.full((1, self.k), 0), axis=0)
-        self.W = np.append(self.W, np.full((self.k, 1), 0), axis=1)
-        self.bu = np.append(self.bu, 0)
-        self.bi = np.append(self.bi, 0)
+        self.H = tf.concat([self.H, tf.fill((1, self.k), 0)], axis=0)
+        self.W = tf.concat([self.W, tf.fill((self.k, 1), 0)], axis=1)
 
     def train(self,
               train_dataset: TrainDataset,
@@ -162,16 +178,17 @@ class MatrixFactoriser(ModelBase):
               user_bias_reg: float = 0.01,
               item_bias_reg: float = 0.01,
               user_reg: float = 0.01,
-              item_reg: float = 0.01):
+              item_reg: float = 0.01) -> List[Evaluation]:
         """For debug mode - call train step when using trainer"""
 
         self.setup_model(train_dataset)
         eval_history = []
+
         # Training epochs
         with EpochBar('Training Step', max=epochs) as bar:
             for epoch in range(epochs):
-                evaluation = self.train_step(train_dataset, eval_dataset, lr, batch_size, user_bias_reg, item_bias_reg,
-                                             user_reg, item_reg)
+                evaluation = self.train_step(
+                    train_dataset, eval_dataset, lr, batch_size, user_bias_reg, item_bias_reg, user_reg, item_reg)
                 if eval_dataset is not None:
                     eval_history.append(evaluation)
                     bar.mse = evaluation.mse
@@ -188,39 +205,29 @@ class MatrixFactoriser(ModelBase):
                    user_bias_reg: float = 0.01,
                    item_bias_reg: float = 0.01,
                    user_reg: float = 0.01,
-                   item_reg: float = 0.01):
+                   item_reg: float = 0.01) -> Union[None, Evaluation]:
 
         if self.R is None:
             self.setup_model(train_dataset)
 
-        user_indices = self.R.users_ratings[:, 0].astype(np.int32)
-        item_indices = self.R.users_ratings[:, 1].astype(np.int32)
-        ratings = self.R.users_ratings[:, 2].astype(np.float32)
-        user_indices_inv_counts = indices_to_inv_counts(user_indices)
-        item_indices_inv_counts = indices_to_inv_counts(item_indices)
+        user_indices, item_indices, ratings = self.R.get_ratings_tensors()
+        user_indices_inv_counts = self.indices_to_inv_counts(user_indices)
+        item_indices_inv_counts = self.indices_to_inv_counts(item_indices)
 
         do_train_step(
-            user_indices,
-            item_indices,
-            ratings,
-            user_indices_inv_counts,
-            item_indices_inv_counts,
-            self.mu,
-            self.bu,
-            self.bi,
-            self.H,
-            self.W,
-            batch_size,
-            lr,
-            user_bias_reg,
-            item_bias_reg,
-            user_reg,
-            item_reg,
+            user_indices, item_indices, ratings, user_indices_inv_counts, item_indices_inv_counts, self.mu, self.bu,
+            self.bi, self.H, self.W, batch_size, lr, user_bias_reg, item_bias_reg, user_reg, item_reg,
         )
 
         # Evaluate at the end of the epoch
         if eval_dataset is not None:
             return self.eval(eval_dataset)
+
+    @staticmethod
+    def indices_to_inv_counts(indices):
+        # Map indices to an array of 1/<index_count>
+        _, inv, counts = np.unique(indices, return_inverse=True, return_counts=True)
+        return tf.convert_to_tensor([1 / counts[i] for i in inv])
 
     def predict(self, dataset: TestDataset) -> np.ndarray:
 
